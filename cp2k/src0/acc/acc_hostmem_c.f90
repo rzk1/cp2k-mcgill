eac8b9c55b16cf3b4f8b19ad43da3689b0487ac5
Rustam Z. Khaliullin <rustam@khaliullin.com>
2017-03-29 08:26:09 -0400

    src0 added back

---
+++ cp2k/src0/acc/acc_hostmem_c.f90
@@ -0,0 +1,188 @@
+!-----------------------------------------------------------------------------!
+!   CP2K: A general program to perform molecular dynamics simulations         !
+!   Copyright (C) 2000 - 2014 the CP2K developers group                       !
+!-----------------------------------------------------------------------------!
+
+
+! *****************************************************************************
+!> \brief Allocates 1D fortan-array as cuda host-pinned memory.
+!> \param host_mem pointer to array
+!> \param n size given in terms of item-count (not bytes!)
+!> \author  Ole Schuett
+! *****************************************************************************
+  SUBROUTINE acc_hostmem_alloc_c (host_mem, n, stream)
+    COMPLEX(kind=real_4), DIMENSION(:), POINTER           :: host_mem
+    INTEGER, INTENT(IN)                      :: n
+    TYPE(acc_stream_type), INTENT(IN)        :: stream
+#if defined (__ACC)
+    TYPE(C_PTR)                              :: host_mem_c_ptr
+
+    CALL acc_hostmem_alloc_raw(host_mem_c_ptr, MAX(1,n)*(2*real_4_size), stream)
+    CALL C_F_POINTER (host_mem_c_ptr, host_mem, (/ MAX(1,n) /))
+#else
+    STOP "acc_hostmem_alloc_c: ACC not compiled in."
+#endif
+  END SUBROUTINE acc_hostmem_alloc_c
+
+
+
+! *****************************************************************************
+!> \brief Allocates 2D fortan-array as cuda host-pinned memory.
+!> \param host_mem pointer to array
+!> \param n1 sizes given in terms of item-count (not bytes!)
+!> \param n2 sizes given in terms of item-count (not bytes!)
+!> \author  Ole Schuett
+! *****************************************************************************
+  SUBROUTINE acc_hostmem_alloc_c_2D (host_mem, n1, n2, stream)
+    COMPLEX(kind=real_4), DIMENSION(:,:), POINTER           :: host_mem
+    INTEGER, INTENT(IN)                      :: n1, n2
+    TYPE(acc_stream_type), INTENT(IN)        :: stream
+#if defined (__ACC)
+    TYPE(C_PTR)                              :: host_mem_c_ptr
+    INTEGER                                  :: n_bytes
+
+    n_bytes = MAX(1,n1)*MAX(1,n2)*(2*real_4_size)
+    CALL acc_hostmem_alloc_raw(host_mem_c_ptr, n_bytes, stream)
+    CALL C_F_POINTER (host_mem_c_ptr, host_mem, (/ MAX(1,n1),MAX(1,n2) /))
+#else
+    STOP "acc_hostmem_alloc_c_2D: ACC not compiled in."
+#endif
+  END SUBROUTINE acc_hostmem_alloc_c_2D
+
+
+! *****************************************************************************
+!> \brief Allocates 3D fortan-array as cuda host-pinned memory.
+!> \param host_mem pointer to array
+!> \param n1 sizes given in terms of item-count (not bytes!)
+!> \param n2 sizes given in terms of item-count (not bytes!)
+!> \param n3 sizes given in terms of item-count (not bytes!)
+!> \author  Ole Schuett
+! *****************************************************************************
+  SUBROUTINE acc_hostmem_alloc_c_3D (host_mem, n1, n2, n3, stream)
+    COMPLEX(kind=real_4), DIMENSION(:,:,:), POINTER           :: host_mem
+    INTEGER, INTENT(IN)                      :: n1, n2, n3
+    TYPE(acc_stream_type), INTENT(IN)        :: stream
+#if defined (__ACC)
+    TYPE(C_PTR)                              :: host_mem_c_ptr
+    INTEGER                                  :: n_bytes
+
+    n_bytes = MAX(1,n1)*MAX(1,n2)*MAX(1,n3)*(2*real_4_size)
+    CALL acc_hostmem_alloc_raw(host_mem_c_ptr, n_bytes, stream)
+    CALL C_F_POINTER (host_mem_c_ptr, host_mem, &
+                               (/ MAX(1,n1),MAX(1,n2),MAX(1,n3) /))
+#else
+    STOP "acc_hostmem_alloc_c_3D: ACC not compiled in."
+#endif
+  END SUBROUTINE acc_hostmem_alloc_c_3D
+
+
+! *****************************************************************************
+!> \brief Allocates 4D fortan-array as cuda host-pinned memory.
+!> \param host_mem pointer to array
+!> \param n1 sizes given in terms of item-count (not bytes!)
+!> \param n2 sizes given in terms of item-count (not bytes!)
+!> \param n3 sizes given in terms of item-count (not bytes!)
+!> \param n4 sizes given in terms of item-count (not bytes!)
+!> \author  Ole Schuett
+! *****************************************************************************
+  SUBROUTINE acc_hostmem_alloc_c_4D (host_mem, n1, n2, n3, n4, stream)
+    COMPLEX(kind=real_4), DIMENSION(:,:,:,:), POINTER           :: host_mem
+    INTEGER, INTENT(IN)                      :: n1, n2, n3, n4
+    TYPE(acc_stream_type), INTENT(IN)        :: stream
+#if defined (__ACC)
+    TYPE(C_PTR)                              :: host_mem_c_ptr
+    INTEGER                                  :: n_bytes
+
+    n_bytes = MAX(1,n1)*MAX(1,n2)*MAX(1,n3)*MAX(1,n4)*(2*real_4_size)
+    CALL acc_hostmem_alloc_raw(host_mem_c_ptr, n_bytes, stream)
+    CALL C_F_POINTER (host_mem_c_ptr, host_mem, &
+                               (/ MAX(1,n1),MAX(1,n2),MAX(1,n3),MAX(1,n4) /))
+#else
+    STOP "acc_hostmem_alloc_c_4D: ACC not compiled in."
+#endif
+  END SUBROUTINE acc_hostmem_alloc_c_4D
+
+
+
+! *****************************************************************************
+!> \brief Deallocates a 1D fortan-array, which is cuda host-pinned memory.
+!> \param host_mem pointer to array
+!> \author  Ole Schuett
+! *****************************************************************************
+  SUBROUTINE acc_hostmem_dealloc_c (host_mem, stream)
+    COMPLEX(kind=real_4), DIMENSION(:), &
+      POINTER                                :: host_mem
+    TYPE(acc_stream_type), INTENT(IN)        :: stream
+    CHARACTER(len=*), PARAMETER :: routineN = 'acc_hostmem_dealloc_c', &
+      routineP = moduleN//':'//routineN
+
+    IF (SIZE (host_mem) == 0) RETURN
+#if defined (__ACC)
+    CALL acc_hostmem_dealloc_raw(C_LOC(host_mem(1)), stream)
+#else
+    STOP "acc_hostmem_dealloc_c: ACC not compiled in."
+#endif
+  END SUBROUTINE acc_hostmem_dealloc_c
+
+
+! *****************************************************************************
+!> \brief Deallocates a 2D fortan-array, which is cuda host-pinned memory.
+!> \param host_mem pointer to array
+!> \author  Ole Schuett
+! *****************************************************************************
+  SUBROUTINE acc_hostmem_dealloc_c_2D (host_mem, stream)
+    COMPLEX(kind=real_4), DIMENSION(:,:), &
+      POINTER                                :: host_mem
+    TYPE(acc_stream_type), INTENT(IN)        :: stream
+    CHARACTER(len=*), PARAMETER :: routineN = 'acc_hostmem_dealloc_c_2D', &
+      routineP = moduleN//':'//routineN
+
+    IF (SIZE (host_mem) == 0) RETURN
+#if defined (__ACC)
+    CALL acc_hostmem_dealloc_raw(C_LOC(host_mem(1,1)), stream)
+#else
+    STOP "acc_hostmem_dealloc_c: ACC not compiled in."
+#endif
+  END SUBROUTINE acc_hostmem_dealloc_c_2D
+
+
+! *****************************************************************************
+!> \brief Deallocates a 3D fortan-array, which is cuda host-pinned memory.
+!> \param host_mem pointer to array
+!> \author  Ole Schuett
+! *****************************************************************************
+  SUBROUTINE acc_hostmem_dealloc_c_3D (host_mem, stream)
+    COMPLEX(kind=real_4), DIMENSION(:,:,:), &
+      POINTER                                :: host_mem
+    TYPE(acc_stream_type), INTENT(IN)        :: stream
+    CHARACTER(len=*), PARAMETER :: routineN = 'acc_hostmem_dealloc_c_3D', &
+      routineP = moduleN//':'//routineN
+
+    IF (SIZE (host_mem) == 0) RETURN
+#if defined (__ACC)
+    CALL acc_hostmem_dealloc_raw(C_LOC(host_mem(1,1,1)), stream)
+#else
+    STOP "acc_hostmem_dealloc_c: ACC not compiled in."
+#endif
+  END SUBROUTINE acc_hostmem_dealloc_c_3D
+
+
+! *****************************************************************************
+!> \brief Deallocates a 4D fortan-array, which is cuda host-pinned memory.
+!> \param host_mem pointer to array
+!> \author  Ole Schuett
+! *****************************************************************************
+  SUBROUTINE acc_hostmem_dealloc_c_4D (host_mem, stream)
+    COMPLEX(kind=real_4), DIMENSION(:,:,:,:), &
+      POINTER                                :: host_mem
+    TYPE(acc_stream_type), INTENT(IN)        :: stream
+    CHARACTER(len=*), PARAMETER :: routineN = 'acc_hostmem_dealloc_c_4D', &
+      routineP = moduleN//':'//routineN
+
+    IF (SIZE (host_mem) == 0) RETURN
+#if defined (__ACC)
+    CALL acc_hostmem_dealloc_raw(C_LOC(host_mem(1,1,1,1)), stream)
+#else
+    STOP "acc_hostmem_dealloc_c: ACC not compiled in."
+#endif
+  END SUBROUTINE acc_hostmem_dealloc_c_4D
---
+++ cp2k/src0/swarm/swarm_message_i8_1d.f90
@@ -0,0 +1,69 @@
+! *****************************************************************************
+!> \brief Addes an entry from a swarm-message.
+!> \param msg ...
+!> \param key ...
+!> \param value ...
+!> \author Ole Schuett
+! *****************************************************************************
+ SUBROUTINE swarm_message_add_i8_1d(msg, key, value)
+   TYPE(swarm_message_type), INTENT(INOUT)   :: msg
+   CHARACTER(LEN=*), INTENT(IN)              :: key
+   INTEGER(KIND=int_8), DIMENSION(:) , INTENT(IN)                        :: value
+
+   TYPE(message_entry_type), POINTER :: new_entry
+
+   IF(swarm_message_haskey(msg, key)) &
+      CALL mp_abort("swarm_message_add_i8_1d: key already exists: "//TRIM(key))
+
+   ALLOCATE(new_entry)
+   new_entry%key = key
+
+   ALLOCATE(new_entry%value_i8_1d(SIZE(value)))
+
+   new_entry%value_i8_1d = value
+
+   !WRITE (*,*) "swarm_message_add_i8_1d: key=",key, " value=",new_entry%value_i8_1d
+
+   IF(.NOT. ASSOCIATED(msg%root)) THEN
+      msg%root => new_entry
+   ELSE
+      new_entry%next => msg%root
+      msg%root => new_entry
+   ENDIF
+
+ END SUBROUTINE swarm_message_add_i8_1d
+
+
+! *****************************************************************************
+!> \brief Returns an entry from a swarm-message.
+!> \param msg ...
+!> \param key ...
+!> \param value ...
+!> \author Ole Schuett
+! *****************************************************************************
+ SUBROUTINE swarm_message_get_i8_1d(msg, key, value)
+   TYPE(swarm_message_type), INTENT(IN)  :: msg
+   CHARACTER(LEN=*), INTENT(IN)          :: key
+   INTEGER(KIND=int_8), DIMENSION(:), POINTER                             :: value
+
+   TYPE(message_entry_type), POINTER :: curr_entry
+   !WRITE (*,*) "swarm_message_get_i8_1d: key=",key
+
+   IF(ASSOCIATED(value)) CALL mp_abort("swarm_message_get_i8_1d: value already associated")
+
+   curr_entry => msg%root
+   DO WHILE(ASSOCIATED(curr_entry))
+      IF(TRIM(curr_entry%key) == TRIM(key)) THEN
+         IF(.NOT. ASSOCIATED(curr_entry%value_i8_1d)) &
+            CALL mp_abort("swarm_message_get_i8_1d: value not associated key: "//TRIM(key))
+         ALLOCATE(value(SIZE(curr_entry%value_i8_1d)))
+         value = curr_entry%value_i8_1d
+         !WRITE (*,*) "swarm_message_get_i8_1d: value=",value
+         RETURN
+      ENDIF
+      curr_entry => curr_entry%next
+   END DO
+   CALL mp_abort("swarm_message_get: key not found: "//TRIM(key))
+ END SUBROUTINE swarm_message_get_i8_1d
+
+!EOF
